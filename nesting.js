d7b75acd994fb909869bb0ff6d4e1e7e53f3b50d
Benjamin Dehlin <ben.dehlin@gmail.com>
2019-12-20 09:11:11 -0700

    nesting project complete

--- nesting.js
+++ nesting.js
@@ -51,6 +51,17 @@ var employees = [
 */
 
 //Code Here
+function employeeUpdater(){
+  for(i = 0; i < employees.length; i++){
+    if(employees[i].firstName === 'Theo'){
+      employees.splice(i, 1)
+    }
+    if(employees[i].firstName === 'Lorie'){
+      employees[i].department = 'HR'
+    }
+  }
+  return employees
+}
 
 
 
@@ -69,7 +80,16 @@ var workplaceAccidents = [12, 56, 44, 3, 29, 56, 56, 3, 7, 12];
 */
 
 //Code Here
-
+function removeDuplicates(){
+  for(i = 0; i < workplaceAccidents.length; i++){
+    for(i2 = 0; i2 < workplaceAccidents.length; i2++){
+      if(workplaceAccidents[i] === workplaceAccidents[i2] && i !== i2){
+        workplaceAccidents.splice(i2, 1)
+      }
+    }
+  }
+  return workplaceAccidents
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -97,8 +117,8 @@ var cat = {
 */
 
 //Code Here
-var grumpyActivity;
-var fluffy2ndFriend;
+var grumpyActivity = cat.catFriends[0].activities[1];
+var fluffy2ndFriend = cat.catFriends[1].name;
 
 
 
@@ -139,7 +159,11 @@ var myCar = {
 */
 
 //Code Here
-
+function recordCleaner(){
+  for(i = 0; i < myCar.accidents.length; i++){
+    myCar.accidents[i].atFaultForAccident = false
+  }
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -159,4 +183,15 @@ var numsArr = [ [1, 2, 3, 4], [5, 6], [7, 8, 9, 10, 11]];
 
 //Code Here
 
-
+function looper(){
+  for(i = 0; i < numsArr.length; i++){
+    for(i2 = 0; i2 < numsArr[i].length; i2++){
+      if(numsArr[i][i2] % 2 === 0){
+        numsArr[i][i2] = 'even'
+      }else{
+        numsArr[i][i2] = 'odd'
+      }
+    }
+  }
+  return numsArr
+}
